e920102278a242d9f58251e70cba3962763320e1
Alex Kim <45559664+Alex-K1m@users.noreply.github.com>
2021-05-29 22:54:24 +0500

    fix codestyle issues
    caused by the eslint-plugin-import update

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,7 +3,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { merge } = require('webpack-merge');
 
 const paths = require('./paths');
-const common = require('./webpack.common.js');
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'production',
